0a68a4728b1e87a614eaa79c74af9b3d4e2df567
Phuong Nguyen <phngyn2@gmail.com>
2022-03-20 18:53:01 -0700

    Reformatting code to call use subprocess for map dictionary creation

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import pyautogui
 import time
+import pyautogui
+import pydirectinput
 
 def screen_bot():
 
@@ -24,15 +25,16 @@ def click_newgame():
     # ok_button = './marks/ok.jpg'
     # ok_loc = pyautogui.locateCenterOnScreen(ok_button)
     # print(ok_loc)
-    pyautogui.click(x=1650+25, y=570+30, clicks=2, interval=1, button='left')
-
+    pydirectinput.click(x=1650+25, y=570+30, clicks=2, interval=1, button='left')
+    pyautogui.press('p')
 
 def click_ok_newgame():
     # ok_button = './marks/ok.jpg'
     # ok_loc = pyautogui.locateCenterOnScreen(ok_button)
     # print(ok_loc)
     #  
-    pyautogui.click(x=1850+25, y=835+30, clicks=2, interval=1, button='left')
+    pydirectinput.click(x=1850+25, y=835+30, clicks=2, interval=1, button='left')
+    pyautogui.press('p')
 
 if __name__ == '__main__':
 
